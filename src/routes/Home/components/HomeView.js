2e8d6f6a60788ae8409a2cc0e5be682ac8974ffe
belfast <liqiang372@gmail.com>
2017-01-01 00:37:32 -0800

    add semantic-ui and basic text area

--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -1,15 +1,36 @@
-import React from 'react'
-import DuckImage from '../assets/Duck.jpg'
+import React, { Component }from 'react'
+import { Grid, Button, Form, } from 'semantic-ui-react'
 import './HomeView.scss'
 
-export const HomeView = () => (
-  <div>
-    <h4>Welcome!</h4>
-    <img
-      alt='This is a duck, because Redux!'
-      className='duck'
-      src={DuckImage} />
-  </div>
-)
-
+class HomeView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: ""
+    } 
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+  }
+  handleSubmit(event) {
+    event.preventDefault()
+    console.log("hello there, I am submitted")
+  }
+  handleChange(event) {
+    this.setState({
+      value: event.target.value
+    })
+  }
+  render() {
+    return(
+      <Grid columns={2} centered>
+        <Grid.Column>
+          <Form onSubmit={this.handleSubmit}>
+           <Form.TextArea name='details' label='Details' placeholder='whats happening!' rows='3' />
+           <Button primary type='submit'>Submit</Button>
+          </Form>
+        </Grid.Column>
+      </Grid>
+    )
+  }
+}
 export default HomeView
